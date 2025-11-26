1b01d098a17bb5e2b6fd5249e9c2bfd8febb35ae
Zash60 <xath06@gmail.com>
2025-11-26 17:43:35 -0300

    Create BuildResult.kt

---
+++ app/src/main/kotlin/com/androidide/model/BuildResult.kt
@@ -0,0 +1,33 @@
+package com.androidide.model
+
+data class BuildResult(
+    val success: Boolean,
+    val apkPath: String? = null,
+    val errors: List<BuildError> = emptyList(),
+    val warnings: List<BuildWarning> = emptyList(),
+    val duration: Long = 0
+)
+
+data class BuildError(
+    val file: String,
+    val line: Int,
+    val column: Int,
+    val message: String
+)
+
+data class BuildWarning(
+    val file: String,
+    val line: Int,
+    val message: String
+)
+
+enum class BuildStep {
+    PREPARING,
+    COMPILING_RESOURCES,
+    COMPILING_KOTLIN,
+    COMPILING_JAVA,
+    CREATING_DEX,
+    PACKAGING,
+    SIGNING,
+    DONE
+}
