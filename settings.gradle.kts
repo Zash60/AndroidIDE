f19e4e445251b5f28b65d9e4fce89fcadb0ec72c
Zash60 <xath06@gmail.com>
2025-11-27 08:49:24 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    // Garante que todos os módulos usem esses repositórios
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
