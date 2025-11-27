f09c0838b716ad69f7428670be73a4f441b1969f
Zash60 <xath06@gmail.com>
2025-11-27 08:50:02 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,12 @@ buildscript {
         google()
         mavenCentral()
     }
+    dependencies {
+        // Classpath para ferramentas de build, se necessário
+    }
 }
 
+// Plugins globais
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
