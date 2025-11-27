16fab9c7dd2953cf650a8725b252e87e2da9670e
Zash60 <xath06@gmail.com>
2025-11-27 08:36:31 -0300

    Update DraculaColorScheme.kt

--- app/src/main/kotlin/com/androidide/ui/editor/DraculaColorScheme.kt
+++ app/src/main/kotlin/com/androidide/ui/editor/DraculaColorScheme.kt
@@ -6,7 +6,7 @@ import io.github.rosemoe.sora.widget.EditorColorScheme
 class DraculaColorScheme : EditorColorScheme() {
 
     init {
-        // Cores Base do Editor (Fundo escuro, texto claro)
+        // Cores Base do Editor
         setColor(WHOLE_BACKGROUND, Color.parseColor("#282A36"))
         setColor(TEXT_NORMAL, Color.parseColor("#F8F8F2"))
         setColor(LINE_NUMBER_BACKGROUND, Color.parseColor("#282A36"))
@@ -18,16 +18,16 @@ class DraculaColorScheme : EditorColorScheme() {
         setColor(BLOCK_LINE_CURRENT, Color.parseColor("#44475A"))
         setColor(NON_PRINTABLE_CHAR, Color.parseColor("#6272A4"))
         
-        // Cores de Sintaxe (Tokens)
-        setColor(OPERATOR, Color.parseColor("#FF79C6"))       // =, +, -, etc (Rosa)
-        setColor(KEYWORD, Color.parseColor("#FF79C6"))        // fun, val, class (Rosa)
-        setColor(LITERAL, Color.parseColor("#BD93F9"))        // true, false, null (Roxo)
-        setColor(ANNOTATION, Color.parseColor("#F1FA8C"))     // @Override (Amarelo)
-        setColor(ATTRIBUTE_NAME, Color.parseColor("#50FA7B")) // android:text (Verde)
-        setColor(ATTRIBUTE_VALUE, Color.parseColor("#F1FA8C"))// "Hello" (Amarelo)
-        setColor(COMMENT, Color.parseColor("#6272A4"))        // Comentários (Cinza Azulado)
-        setColor(HTML_TAG, Color.parseColor("#FF79C6"))       // <View> (Rosa)
-        setColor(FUNCTION_NAME, Color.parseColor("#8BE9FD"))  // method() (Ciano)
-        setColor(IDENTIFIER_NAME, Color.parseColor("#F8F8F2"))// Variaveis
+        // Cores de Sintaxe
+        setColor(OPERATOR, Color.parseColor("#FF79C6"))
+        setColor(KEYWORD, Color.parseColor("#FF79C6"))
+        setColor(LITERAL, Color.parseColor("#BD93F9"))
+        setColor(ANNOTATION, Color.parseColor("#F1FA8C"))
+        setColor(ATTRIBUTE_NAME, Color.parseColor("#50FA7B"))
+        setColor(ATTRIBUTE_VALUE, Color.parseColor("#F1FA8C"))
+        setColor(COMMENT, Color.parseColor("#6272A4"))
+        setColor(HTML_TAG, Color.parseColor("#FF79C6"))
+        setColor(FUNCTION_NAME, Color.parseColor("#8BE9FD"))
+        setColor(IDENTIFIER_NAME, Color.parseColor("#F8F8F2"))
     }
 }
