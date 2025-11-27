0570b64f044f252dc66cbcbe6679d0afdbaea3cc
Zash60 <xath06@gmail.com>
2025-11-26 21:35:33 -0300

    Create GitManager.kt

---
+++ app/src/main/kotlin/com/androidide/utils/GitManager.kt
@@ -0,0 +1,51 @@
+package com.androidide.utils
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
+import java.io.File
+
+object GitManager {
+
+    fun cloneRepository(url: String, destDir: File): Boolean {
+        return try {
+            if (destDir.exists()) destDir.deleteRecursively()
+            Git.cloneRepository()
+                .setURI(url)
+                .setDirectory(destDir)
+                .call()
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    fun pull(projectDir: File): Boolean {
+        return try {
+            val git = Git.open(projectDir)
+            git.pull().call()
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    fun commitAndPush(projectDir: File, message: String, token: String): Boolean {
+        return try {
+            val git = Git.open(projectDir)
+            git.add().addFilepattern(".").call()
+            git.commit().setMessage(message).call()
+            
+            // Push requer autenticação (Token do GitHub)
+            if (token.isNotEmpty()) {
+                val credentials = UsernamePasswordCredentialsProvider(token, "")
+                git.push().setCredentialsProvider(credentials).call()
+            }
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+}
